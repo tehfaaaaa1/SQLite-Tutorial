f44f05ee3ceb74434fdc10078ffdfc1253f89f3a
TEHFAAAA <fateehganteng@gmail.com>
2023-10-23 15:01:16 +0700

    Pertama kali SQLITE

--- app/src/main/java/com/example/aplikasistoragesqlite/LoginActivity.kt
+++ app/src/main/java/com/example/aplikasistoragesqlite/LoginActivity.kt
@@ -1,10 +1,13 @@
 package com.example.aplikasistoragesqlite
 
+import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import com.google.android.material.textfield.TextInputEditText
 import java.util.prefs.Preferences
 
@@ -24,5 +27,35 @@ class LoginActivity : AppCompatActivity() {
         val inputUsername: EditText = findViewById(R.id.inputUsername)
         val inputPassword: TextInputEditText = findViewById(R.id.inputPassword)
         val buttonLogin : Button = findViewById(R.id.loginButton)
+
+        sharedPreferences = getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE)
+        email = sharedPreferences.getString("email", null)
+        password = sharedPreferences.getString("password", null)
+
+        buttonLogin.setOnClickListener {
+            if(inputUsername.text.toString().isEmpty() &&
+                inputPassword.text.toString().isEmpty()) {
+                Toast.makeText(this, "Please input username and password", Toast.LENGTH_LONG)
+            }
+            else {
+                val editor = sharedPreferences.edit()
+                editor.putString(EMAIL_KEY, inputUsername.text.toString())
+                editor.putString(PASSWORD_KEY, inputPassword.text.toString())
+
+                editor.apply()
+                val i = Intent(this,MainActivity::class.java)
+                startActivity(i)
+                finish()
+            }
+        }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (email != null && password != null) {
+            val i = Intent(this,MainActivity::class.java)
+            startActivity(i)
+        }
     }
 }
